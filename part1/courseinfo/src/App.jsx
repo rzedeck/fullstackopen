3400005f85b70200ee64668b4ea23ac8832a9cea
Romenique Zedeck <romenique.zedeck@assetmark.com>
2024-09-03 10:50:33 +0100

    Typo fix for solution 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -43,7 +43,7 @@ const App = () => {
       <Header course={course} />
       <Content part1={part1} exercises1={exercises1} 
                part2={part2} exercises2={exercises2}
-               part3={part2} exercises3={exercises3}/>
+               part3={part3} exercises3={exercises3}/>
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
