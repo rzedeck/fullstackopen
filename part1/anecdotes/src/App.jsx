2aef172812564f8ecb11276635690ea5d3a89448
Romenique Zedeck <romenique.zedeck@assetmark.com>
2024-09-20 18:29:02 +0100

    Solution for exercise 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -18,6 +18,7 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(new Uint8Array(anecdotes.length))
+  const [mostVotedIndex, setmostVotedIndex] = useState(0)
 
   const handleNextClick = () => {
     //console.log('Votes array ', votes)
@@ -25,22 +26,30 @@ const App = () => {
     if(randomNum >= 0){
       setSelected(randomNum)
     }else{
-      setSelected(anecdotes.length - 1)//the last joke is used for out of bounds intead the first one [0]
+      setSelected(anecdotes.length - 1)//the last joke is used for out of bounds instead the first one [0]
     }
+    
   }
 
   const handleVoteClick = () => {
     const pointsCopy = [...votes]
     pointsCopy[selected] += 1
     setVotes(pointsCopy)
+    const maxVoteValue = Math.max(...pointsCopy)
+    const maxVoteIndex = pointsCopy.indexOf(maxVoteValue)
+    setmostVotedIndex(maxVoteIndex)
   }
 
   return (
     <div>
+      <h1>Anedocte of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <Button handleClick={handleVoteClick} text='Vote' />
       <Button handleClick={handleNextClick} text='Next Anedocte' />
+      <h1>Anedocte with most votes</h1>
+      <p>{anecdotes[mostVotedIndex]}</p>
+      <p>has {votes[mostVotedIndex]} votes</p>
     </div>
   )
 }
