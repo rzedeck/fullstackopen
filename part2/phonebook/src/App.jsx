0888cf5c3a5e554f66909f08ebcf303db454b4d3
Romenique Zedeck <romenique.zedeck@assetmark.com>
2024-09-27 18:45:47 +0100

    Solution for exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,55 @@
 import { useState } from 'react'
 
+const DisplayContact = ({ contact }) => {
+  //console.log('contact', contact.name)
+  return <p>{contact.name}</p>
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { id: 1, name: 'Arto Hellas' }
+  ])
   const [newName, setNewName] = useState('')
+
+  const addContact = (event) => {
+    event.preventDefault()
+    const contactObject = {
+      id: String(persons.length + 1),
+      name: newName
+    }
+    
+    if(persons.every(person => person.name !== contactObject.name)){
+      setPersons(persons.concat(contactObject))
+      setNewName('')
+    }else{
+      alert(`${contactObject.name} is already added to phonebook`)
+    }
+
+  }
+
+  const handleNameChange = (event) => setNewName(event.target.value)
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addContact}>
         <div>
-          name: <input />
+          name: <input onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <>
+        {persons.map(person => {
+          //console.log(person);
+          return (
+            <DisplayContact key={person.id} contact={person} />
+          );
+        })}
+      </>
+
     </div>
   )
 }
