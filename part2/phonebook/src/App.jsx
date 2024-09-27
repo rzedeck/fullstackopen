31e6276ac49580585fc72ba9390c1d53a97368b8
Romenique Zedeck <romenique.zedeck@assetmark.com>
2024-09-27 19:34:20 +0100

    Solution for exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -7,10 +7,14 @@ const DisplayContact = ({ contact }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas', number: '040-1234567' }
+    { id: 1, name: 'Arto Hellas', number: '040-123456' },
+    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addContact = (event) => {
     event.preventDefault()
@@ -32,10 +36,16 @@ const App = () => {
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setNewFilter(event.target.value)
+
+  const filteredContacts = 
+  persons.filter(person => person.name.toLocaleLowerCase().includes(newFilter.toLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
+      Filter Names with <input onChange={handleFilterChange} />
+      <h2>Add a new contact</h2>
       <form onSubmit={addContact}>
         <div>
           name: <input onChange={handleNameChange}/>
@@ -49,7 +59,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <>
-        {persons.map(person => {
+        {filteredContacts.map(person => {
           //console.log(person);
           return (
             <DisplayContact key={person.id} contact={person} />
