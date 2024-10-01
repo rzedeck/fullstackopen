5ff7f630cd55ea29944b969ba9dac1b23c2970cd
Romenique Zedeck <romenique.zedeck@assetmark.com>
2024-10-01 15:54:50 +0100

    Solution for exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import ContactFilter from './components/ContactFilter'
 import ContactList from './components/ContactList'
 import AddContact from './components/AddContact'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas', number: '040-123456' },
-    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
-    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
-    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+      setPersons(response.data)
+      })
+  }, [])
 
   const addContact = (event) => {
     event.preventDefault()
