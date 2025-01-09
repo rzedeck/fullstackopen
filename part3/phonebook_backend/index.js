873b64ea80d9cde01ef402b09ab74369681a5cc5
Romenique Zedeck <r.zedeck@gmail.com>
2025-01-09 16:21:56 +0000

    Solution for exercises 3.05 and 3.06

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     "id": "1",
@@ -42,6 +44,33 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number is missing' 
+    })
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  const person = {
+    id:  Math.floor(Math.random() * 10000000000000).toString(),
+    name: body.name,
+    number: body.number
+    
+  }
+
+  persons = persons.concat(person)
+  
+  response.json(body)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
