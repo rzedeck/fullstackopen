7d17a1a1c5d4558f757f308723d26e86bcc874cb
Romenique Zedeck <r.zedeck@gmail.com>
2025-01-23 16:24:17 +0000

    Solution for exercise 3.11

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => (req.method === 'POST' ? JSON.stringify(req.body) : ''))
 
