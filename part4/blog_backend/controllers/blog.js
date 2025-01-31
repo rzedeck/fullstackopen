ea4d6d21caed7d4ee9e5bba126f46ed6d77b81db
Romenique Zedeck <r.zedeck@gmail.com>
2025-01-31 17:19:58 +0000

    Solution for exercises 4.01 and 4.02

---
+++ part4/blog_backend/controllers/blog.js
@@ -0,0 +1,59 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get('/', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+})
+
+blogRouter.get('/:id', (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then(blog => {
+      if (blog) {
+        response.json(blog)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+blogRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+
+  blog.save()
+    .then(savedBlog => {
+      response.status(201).json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+blogRouter.delete('/:id', (request, response, next) => {
+  Blog.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+blogRouter.put('/:id', (request, response, next) => {
+  const { title, author, url, likes } = request.body
+
+  Blog.findByIdAndUpdate(request.params.id,
+    { title, author, url, likes },
+    { new: true, runValidators: true, context: 'query' })
+    .then(updatedBlog => {
+      response.json(updatedBlog)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogRouter
